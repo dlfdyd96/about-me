a8e1c6b499217077a8e65637b1292484dd87d2de
dlfdyd96 <1yongs_@naver.com>
2021-07-02 18:37:30 +0900

    FIX: vue 3.1.3 version 에러

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
+  purge: { content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
