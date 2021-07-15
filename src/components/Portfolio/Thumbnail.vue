e1f9f0f66106b7f7be0f990c878578341985f098
dlfdyd96 <1yongs_@naver.com>
2021-07-16 01:17:09 +0900

    ADD: Portfolio thumbnail

---
+++ src/components/Portfolio/Thumbnail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="relative">
+    <!-- <a/> -->
+    <img
+      class="rounded-xl bg-center b-no-repeat bg-auto"
+      ref="pic"
+      :src="require(`@/assets/portfolio/${image}`)"
+      :style="{ filter: `brightness(40%)` }"
+      alt="dd"
+    />
+    <div
+      class="
+        absolute
+        inset-0
+        text-white
+        flex
+        items-center
+        justify-center
+        text-2xl
+      "
+    >
+      {{ title }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    image: String,
+    title: String,
+  },
+});
+</script>
+
+<style></style>
