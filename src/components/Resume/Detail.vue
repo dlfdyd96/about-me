62d6e0562dcd24088ce34133b7e17ec06cd1e4f4
dlfdyd96 <1yongs_@naver.com>
2021-09-16 16:01:30 +0900

    FEAT: 이력서 Info에 여러개 아이템 나열 기능 추가

--- src/components/Resume/Detail.vue
+++ src/components/Resume/Detail.vue
@@ -18,14 +18,21 @@
           </div>
           <div class="col-span-3">
             <!-- description -->
-            <a
-              class="font-medium underline"
-              v-if="value.href"
-              :href="value.href"
-              target="_blank"
-              >{{ value.description }}</a
-            >
-            <span class="font-medium" v-else>{{ value.description }}</span>
+            <span v-for="(item, index) in value.items" :key="index">
+              <a
+                class="font-medium underline"
+                v-if="item.href"
+                :href="item.href"
+                target="_blank"
+                >{{ item.description }}</a
+              >
+              <span class="font-medium" v-else>{{ item.description }}</span>
+              <span
+                v-if="value.items.length > 1 && value.items.length != index + 1"
+                class="mr-1"
+                >,
+              </span>
+            </span>
           </div>
         </div>
       </div>
@@ -38,8 +45,13 @@ import { defineComponent, ref } from "vue";
 
 interface InfoItemI {
   title: string;
-  description: string;
   icon: string;
+  items: DescriptionItemI[];
+  href?: string;
+}
+
+interface DescriptionItemI {
+  description: string;
   href?: string;
 }
 
@@ -60,35 +72,63 @@ export default defineComponent({
       birth: {
         title: `Birth`,
         icon: `fas fa-birthday-cake`,
-        description: "Oct 18th, 1996",
+        items: [
+          {
+            description: "Oct 18th, 1996",
+          },
+        ],
       },
       residence: {
         title: `Blog`,
         icon: `fas fa-newspaper`,
-        description: `日用\`s Blog`,
-        href: "https://dlfdyd96.tistory.com/",
+        items: [
+          {
+            description: `日用\`s Blog`,
+            href: "https://dlfdyd96.tistory.com/",
+          },
+          {
+            description: `velog`,
+            href: "https://velog.io/@1yongs_",
+          },
+        ],
       },
       address: {
         title: `Address`,
         icon: `fas fa-map-marker-alt`,
-        description: `Gwanak-gu, Seoul`,
+        items: [
+          {
+            description: `Gwanak-gu, Seoul`,
+          },
+        ],
       },
       e_mail: {
         title: `E-Mail`,
         icon: `fas fa-paper-plane`,
-        description: `1yongs_@naver.com`,
+        items: [
+          {
+            description: `1yongs_@naver.com`,
+          },
+        ],
       },
       github: {
         title: `github`,
         icon: `fab fa-github`,
-        description: `https://github.com/dlfdyd96`,
-        href: "https://github.com/dlfdyd96",
+        items: [
+          {
+            description: `https://github.com/dlfdyd96`,
+            href: "https://github.com/dlfdyd96",
+          },
+        ],
       },
       employee: {
         title: `Employee`,
         icon: `fas fa-building`,
-        description: `Health And Medicine Corp.`,
-        href: "https://hnmcorp.kr/",
+        items: [
+          {
+            description: `Health And Medicine Corp.`,
+            href: "https://hnmcorp.kr/",
+          },
+        ],
       },
     });
 
